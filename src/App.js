56a9cf89dae23f9f1d0dd60d6fe187ae55586014
vaibhavj <vaibhavj@cdac.in>
2022-06-12 18:24:37 +0530

    Added arrow function syntax

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Food",
-      amount: 99.5,
-      date: new Date(2022, 7, 13),
-    },
-    {
-      id: "e2",
-      title: "Clothing",
-      amount: 959.5,
-      date: new Date(2022, 7, 15),
-    },
-    {
-      id: "e3",
-      title: "Bag",
-      amount: 199.5,
-      date: new Date(2022, 7, 19),
-    },
-    {
-      id: "e4",
-      title: "Bike",
-      amount: 92229.5,
-      date: new Date(2022, 7, 25),
-    },
-  ];
+const App = () => {
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses />
     </div>
   );
-}
+};
 
 export default App;
